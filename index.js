fddfdaebcd15d3017dab589b0a0802683594a923
su-san1129 <deviationvalue1129@icloud.com>
2019-07-14 01:29:54 +0900

    34. 演習問題

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js',
+    url: 'https://jp.vuejs.org/',
+    toggle: true,
+    languages: ['JavaScript','Ruby','Python']
+  },
+  methods: {
+    clickHandler: function(){
+      this.message = 'clicked!'
+    }
+  }
+});
